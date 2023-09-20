2dfad5a39692fd4b32a1617d047288c60e46b2da
Vitor Lopes <vitor.lfigueiredo93@gmail.com>
2023-09-20 11:31:48 -0300

    Melhorias no código

--- lib/bob.dart
+++ lib/bob.dart
@@ -1,14 +1,27 @@
 class Bob {
+  bool isSilence(String question) => question.trim().isEmpty;
+
+  bool isScreaming(String question) {
+    return question == question.toUpperCase() && hasLetters(question);
+  }
+
+  bool hasLetters(String question) =>
+      question.toUpperCase() != question.toLowerCase();
+
+  bool isAQuestion(String question) =>
+      question.lastIndexOf("?") == question.length - 1;
+
   String response(String question) {
-    if (question.substring(question.length - 1) == "?") {
-      return "Sure.";
-    } else if (question.contains(question.toUpperCase()) &&
-        (question.substring(question.length - 1) == "?")) {
-      return "Calm down, I know what I'm doing!";
-    } else if (question.length.isNaN) {
-      return question.toLowerCase();
-    } else if (question.isEmpty || question.indexOf("") >= 0) {
+    var preparedQuestion = question.trim();
+
+    if (isSilence(preparedQuestion)) {
       return "Fine. Be that way!";
+    } else if (isAQuestion(preparedQuestion) && isScreaming(preparedQuestion)) {
+      return "Calm down, I know what I'm doing!";
+    } else if (isScreaming(preparedQuestion)) {
+      return "Whoa, chill out!";
+    } else if (isAQuestion(preparedQuestion)) {
+      return "Sure.";
     } else {
       return "Whatever.";
     }
